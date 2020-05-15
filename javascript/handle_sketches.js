3869b33a0b9edf14abb7476d5bcbb7ee104f54c8
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2020-05-15 16:20:11 +0200

    Finish interface

---
+++ javascript/handle_sketches.js
@@ -0,0 +1,8 @@
+function save_sketch() {
+	if(is_learning == ""){
+		$('.toast').toast('show');
+		return;
+	}
+	
+	console.log(is_learning);
+}
--- javascript/sketch.js
+++ javascript/sketch.js
@@ -1,38 +1,64 @@
-/**
- * TODO : code abc || Lilypond || teX
- * TODO : commenter le code...
- */
- 
-
 const xSize = 100;
 const ySize = 150;
 
-const NEW_MODEL = false;
+var is_learning = "";
 
-const toString = ["TREBLE CLEF", "BASS CLEF", "ALTO CLEF", "C3", "D3", "E3", "F3", "G3", "A3", "B3"];
 
-let features;
-let knn;
-let data_to_train = "";
+function init_interaction(){
 
+	// ENTER KEY
+	document.addEventListener("keyup", function(event) {
+	if (event.keyCode === 13) {
+		save_sketch();
+		background(255);
+		createMusic();
+		}
+	});
+
+	// Submit button
+	$('#submit_sketch').click(function(){
+		save_sketch();
+		background(255);
+		createMusic();
+	});
+
+	$('.example_buttons').on('click', function(event){
+		if(is_learning != event.target.id) {
+			is_learning = event.target.id;
+		}
+		else{
+			is_learning = "";
+		}
+		enhance(is_learning);
+	});
+
+}
+
+function enhance(id){
+	$('.example_buttons').css("background-color", "rgba(0,0,0,0.1)");
+	$('.example_buttons').css("width", "100");
+	$('.example_buttons').css("height", "150");
+
+	if(id == ""){return;}
+
+	var parent = $('#' + id).parent();
+	parent.css("background-color", "rgba(0,150,0,0.2)");
+	parent.css("width", "110px");
+	parent.css("height", "165px");
+
+}
+
+
+// ================= P5.js CODE ======================
 function setup(){
-	// GRAPHICS ----
 	cnv = createCanvas(xSize, ySize);
-	cnv.parent("conteneur");
-	cnv.style("border", "solid");
+	cnv.parent("sketch");
 	background(255);
 	createMusic();
-
-	// NEURAL NETWORK
-	features = ml5.featureExtractor('MobileNet', modelReady);
-	knn = ml5.KNNClassifier();
-	console.log("Neural network initialized");
 }
 
 
 function draw() {
-	data_to_train = document.getElementById("select_item").value;
-	is_training = document.getElementById("to_train").checked;
 	if (mouseIsPressed === true) {
 		line(mouseX, mouseY, pmouseX, pmouseY);
     }
@@ -46,70 +72,4 @@ function createMusic() {
 	for(var i = 0 ; i < 5 ; i++){
 		line(10, begin + i*gap + gap, xSize - 10, begin + i*gap + gap);
 	}
-}
-
-
-function modelReady() {
-  if(!NEW_MODEL){
-  		document.getElementById("to_train").checked = false;
-		knn.load('model.json', function() {
-			console.log('KNN loaded');
-		});
-	}
-
-	else{
-		document.getElementById("to_train").checked = true;
-	}
-
-	console.log("MODEL INITIALZED"); 
-}
-
-function gotResult(error, result){
-	if(error){
-		console.log(error);
-		return;
-	}
-	if(NEW_MODEL){
-		console.log(toString[result.classIndex]);
-	}
-	else{
-		console.log(toString[parseInt(result.label)]);
-	}
-} 
-
-
-
-function mouseReleased(){
-	if(knn.getNumLabels() > 0 && !is_training){
-		const logits = features.infer(cnv);
-		knn.classify(logits, gotResult);
-		background(255);
-		createMusic();
-	}
-}
-
-
-
-
-function keyPressed(){
-	const logits = features.infer(cnv);
-
-	if(key == 't'){
-		knn.addExample(logits, data_to_train);
-		console.log(data_to_train);
-	}
-
-	else if(key == 'z'){
-		console.log("Cancel input");
-	}
-
-	else if(key == 's'){
-		knn.save("model.json")
-	}
-
-
-	background(255);
-	createMusic();
-
-
 }
