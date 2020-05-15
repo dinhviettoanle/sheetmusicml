ee7cc5311097f41d594eb89cabb454692218ed9f
dinhviettoanle <le.dinh.viet.toan@gmail.com>
2020-05-15 19:16:45 +0200

    Add verifications

--- javascript/handle_sketches.js
+++ javascript/handle_sketches.js
@@ -1,8 +1,13 @@
+var total_size = 0;
+
 function save_sketch() {
 	if(is_learning == ""){
 		$('.toast').toast('show');
 		return;
 	}
+
+
+	// TODO : Do something to minimize the length of all data
 
 	loadPixels();
 	var this_sketch = [];
@@ -13,9 +18,66 @@ function save_sketch() {
 		}
 	}
 
-	sketches.push(this_sketch);
+
+	// Verify that the sketch is not empty
+	is_empty = true;
+
+	for(var i = 0 ; i < this_sketch.length ; i++){
+		if (this_sketch[i] !== empty_sketch[i]){
+			is_empty = false;
+			break;
+		}
+	}
+
+
+	if(is_empty){
+		$('.toast').toast('show');
+		return;
+	}
+
+
+	sketches.push({'sketch' : this_sketch, 'element' : is_learning});
+
+	total_size += byteLength(JSON.stringify(this_sketch));
+
+	$('#size_file').text(Math.round(total_size/1000).toString());
 	background(255);
 	image(img, 0, 0);
 
-	console.log(sketches);
+}
+
+function byteLength(str) {
+  // returns the byte length of an utf8 string
+  var s = str.length;
+  for (var i=str.length-1; i>=0; i--) {
+    var code = str.charCodeAt(i);
+    if (code > 0x7f && code <= 0x7ff) s++;
+    else if (code > 0x7ff && code <= 0xffff) s+=2;
+    if (code >= 0xDC00 && code <= 0xDFFF) i--; //trail surrogate
+  }
+  return s;
+}
+
+
+
+function send_data() {
+	const json_file_content = JSON.stringify({'data' : sketches, 'total' : sketches.length});
+	download("test.json", json_file_content);
+	sketches = [];
+}
+
+
+
+
+function download(filename, text) {
+  var element = document.createElement('a');
+  element.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(text));
+  element.setAttribute('download', filename);
+
+  element.style.display = 'none';
+  document.body.appendChild(element);
+
+  element.click();
+
+  document.body.removeChild(element);
 }
--- javascript/sketch.js
+++ javascript/sketch.js
@@ -2,8 +2,10 @@ const XSIZE = 100;
 const YSIZE = 150;
 
 var is_learning = "";
+var prec_is_learning = "";
 
 var sketches = [];
+var empty_sketch = []
 
 
 
@@ -21,30 +23,38 @@ function init_interaction(){
 			}
 	});
 
-	// Submit button
+	// Submit button to create a sketch
 	$('#submit_sketch').click(function(){
 		save_sketch();
-		background(255);
-		image(img, 0, 0);
 	});
 
 	// Select element
 	$('.example_buttons').on('click', function(event){
 		if(is_learning != event.target.id) {
+			prec_is_learning = is_learning;
 			is_learning = event.target.id;
 		}
 		else{
 			is_learning = "";
 		}
-		enhance(is_learning);
+		enhance(is_learning, prec_is_learning);
+	});
+
+
+	// Send data
+	$('#send_button').click(function(){
+		send_data();
 	});
 
 }
 
 function enhance(id){
-	$('.example_buttons').css("background-color", "rgba(0,0,0,0.1)");
-	$('.example_buttons').css("width", "100");
-	$('.example_buttons').css("height", "150");
+	if(prec_is_learning != ""){
+		var parent_prec = $('#' + prec_is_learning).parent();
+		parent_prec.css("background-color", "");
+		parent_prec.css("width", "");
+		parent_prec.css("height", "");
+	}
 
 	if(id == ""){return;}
 
@@ -66,7 +76,15 @@ function setup(){
 	cnv.parent("sketch");
 	background(255);
 	image(img, 0, 0);
-	// createMusic();
+
+	loadPixels();
+	for(y = 0 ; y < YSIZE ; y++){
+		for(x = 0 ; x < XSIZE ; x++){
+			var index = (x + y * XSIZE) * 4;
+			empty_sketch.push(pixels[index + 1]);
+		}
+	}
+
 }
 
 
